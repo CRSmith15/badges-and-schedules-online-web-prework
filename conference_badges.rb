d7181acf53e66f82218665bfb8a12b00bb1c7735
Chance Smith <chancesmith97@hotmail.com>
2019-02-04 00:09:49 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,6 +3,6 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(attendees)
-  attendees.each { |x| return "Hello my name is #{name}."}
+  attendees.each { |x| return "Hello my name is #{attendees}."}
 end
   
